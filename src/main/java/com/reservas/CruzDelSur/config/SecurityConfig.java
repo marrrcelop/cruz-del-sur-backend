141a2a97183fa53681d1df72071e207e004b899a
Tu Nombre <tuemail@example.com>
2026-07-13 11:06:16 -0500

    Agrega permiso a la pagina

--- src/main/java/com/reservas/CruzDelSur/config/SecurityConfig.java
+++ src/main/java/com/reservas/CruzDelSur/config/SecurityConfig.java
@@ -68,7 +68,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:4200"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://127.0.0.1:4200",
+                "https://stately-chaja-5a3d6b.netlify.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
